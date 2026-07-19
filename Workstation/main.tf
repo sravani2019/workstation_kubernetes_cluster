88dfde0b9634769a454f70d8b2d7bdaf3d7bd2ea
sravani192019 <sravani.jamithireddy@gmail.com>
2026-07-19 08:36:16 +0530

    cluster

--- Workstation/main.tf
+++ Workstation/main.tf
@@ -13,38 +13,26 @@ resource "aws_instance" "workstation" {
     delete_on_termination = true
   
   }
- 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo dnf -y install dnf-plugins-core",
-      "sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo",
-      "sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
-      "sudo systemctl enable --now docker",
-      "sudo usermod -aG docker ec2-user",
-      # Detect system architecture and fetch the corresponding binary
-      "ARCH=$(uname -m | sed 's/x86_64/amd64/' | sed 's/aarch64/arm64/')",
-      "PLATFORM=$(uname -s)_$ARCH",
-      "curl -sLO https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz",
-      "curl -sL https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt | grep $PLATFORM | sha256sum --check",
-      "tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz",
-      "sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl",
-      "curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl",
-      "chmod +x ./kubectl",
-      "sudo mv kubectl /usr/local/bin/kubectl",
-
-    ]
+  tags = {
+    Name = "workstation"
   }
+}
+  #  provisioner "remote-exec" {
+    
+   
+
+  #  }
   
-     connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ec2-user"
-      password = "DevOps321"
-      timeout     = "5m"
-    }
+  #    connection {
+  #     type        = "ssh"
+  #     host        = self.public_ip
+  #     user        = "ec2-user"
+  #     password = "DevOps321"
+  #     timeout     = "5m"
+  #   }
 
-     tags = var.ec2_tags
-  }
+  #    tags = var.ec2_tags
+  # }
 
 
 
